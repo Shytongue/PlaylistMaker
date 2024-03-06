989d678407d862e36ffab47c320efd124c5b8aae
Shytongue <fieryfire@mail.ru>
2024-03-07 02:01:54 +0300

    Экраны MainActivity и SettingsActivity, Intents

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mainSearchButton = findViewById<Button>(R.id.main_search)
+        val mainMediaButton = findViewById<Button>(R.id.main_media)
+        val mainSettingsButton = findViewById<Button>(R.id.main_settings)
+
+        mainSearchButton.setOnClickListener {
+            val searchIntent = Intent(this, SearchActivity::class.java)
+            startActivity(searchIntent)
+        }
+        mainMediaButton.setOnClickListener {
+            val searchIntent = Intent(this, MediaActivity::class.java)
+            startActivity(searchIntent)
+        }
+        mainSettingsButton.setOnClickListener {
+            val searchIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(searchIntent)
+        }
     }
+
 }
